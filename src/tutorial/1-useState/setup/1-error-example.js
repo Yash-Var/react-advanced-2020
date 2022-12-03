b19708f524687e32830633c16219ddbfcbf2d16a
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-12-03 23:51:30 +0530

    hooks

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const ErrorExample = () => {
   let title='random title';
+  const [Title,setTitle]=useState(title);
+  const handleClick=()=>{
+    setTitle="ram varshney";
+  }
   return (
 <>
   <h2>{title}</h2>
+  <button type='button' className='btn' onClick={handleClick}>Change Title</button>
 </>
   );
 };
