1fc3624e58777271563d0e1802b77dca7baa2c37
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-12-06 23:45:11 +0530

    added

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const respose = await fetch(url);
+    const users = await respose.json();
+    console.log(users);
+    setUsers(users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <>
+      <h3>Github Users</h3>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
